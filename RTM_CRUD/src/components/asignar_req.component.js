e8330a5be9a3e2a935200d26416b3e9ff14577c5
Diego Loaiza <dfloaiza@gmail.com>
2022-05-14 14:59:21 -0500

    Create asignar_req.component.js

---
+++ RTM_CRUD/src/components/asignar_req.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AsignarReq extends Component {
+    render() {
+        return (
+            <div>
+                <p>Crear Requerimiento</p>
+            </div>
+        );
+    }
+}
