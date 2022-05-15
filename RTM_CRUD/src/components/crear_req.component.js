5b5ef4f368b37267cfc5908e8ca2f47878b52c41
Diego Loaiza <dfloaiza@gmail.com>
2022-05-14 20:52:33 -0500

    Update crear_req.component.js

--- RTM_CRUD/src/components/crear_req.component.js
+++ RTM_CRUD/src/components/crear_req.component.js
@@ -1,1 +1,11 @@
+import React, { Component } from 'react';
 
+export default class CrearReq extends Component {
+    render() {
+        return (
+            <div>
+                <p>Crear Requerimiento</p>
+            </div>
+        );
+    }
+}
